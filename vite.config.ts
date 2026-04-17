4ba605cc1af606c40d471924eb27b7458834fc19
NikaKhi <xaiminik@gmail.com>
2026-04-18 00:35:33 +0300

    Fix API configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://1e14c3489fcb.vps.myjino.ru:5000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
